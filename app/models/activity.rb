994d889ce720973cd3c6cd03bc5e412d5268bad3
James Kobol <jim.kobol@gmail.com>
2013-07-10 22:38:42 -0600

    have simple form for creating activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,7 +4,7 @@ class Activity < ActiveRecord::Base
 
   belongs_to :user
 
-  attr_accessible :description, :name
+  attr_accessible :description, :name, :tag_list
 
   validates :name, presence: true, length: { minimum: 5 }
   validates :description, presence: true, length: { minimum: 5 }
