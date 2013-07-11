aff8ef03f027a81db0e50ad27cc6dd2ad7a2be3f
James Kobol <jim.kobol@gmail.com>
2013-07-11 09:03:39 -0600

    added activity states

---
+++ app/models/activity_state.rb
@@ -0,0 +1,3 @@
+class ActivityState < ActiveRecord::Base
+  # attr_accessible :title, :body
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,4 @@
 require 'yaml'
-
-
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -14,7 +12,14 @@ require 'yaml'
 
 
 
-
+# Activiyt States
+seed = YAML::load_file('db/seeds/activity_states.yaml')
+seed.each_pair do |key,type|  
+	print "#{key}, #{type}\n"
+	type_object = ActivityState.find_or_create_by_name type['name']
+	type_object.thumbnail = type['thumbnail']
+	type_object.save
+end  
 
 
 
