5fc38ab8d63e0c8a254464dd1bae829fe2390686
James Kobol <jim.kobol@gmail.com>
2013-08-07 15:10:29 -0700

    fixed login template error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,22 +7,17 @@ class SessionsController < ApplicationController
 
   def create
     Rails.logger.info params
-    
-    if request.env.has_key?('omniauth.auth')
-      auth_hash = request.env['omniauth.auth']
-      Rails.logger.info "Auth Hash #{auth_hash.inspect}"
+
+    user = User.find_by_email(params[:session][:name].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      # redirect_to user
+      redirect_to root_path
+    else
+      flash.now[:error] = t 'signin.error'
+      render 'new'
     end
-
-    # user = User.find_by_email(params[:session][:name].downcase)
-
-    # if user && user.authenticate(params[:session][:password])
-    #   sign_in user
-    #   # redirect_to user
-    #   redirect_to root_path
-    # else
-    #   flash.now[:error] = t 'signin.error'
-    #   render 'new'
-    # end
   end
 
   def destroy
