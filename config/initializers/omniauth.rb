e72e1e7933cacb683c0b8e3fee87703bd8dd0e42
James Kobol <jim.kobol@gmail.com>
2013-08-07 13:48:24 -0700

    was able to get callisto rails project running, jeez

--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 
-  				 Rails.application.config.FACEBOOK_APP_ID, 
-  				 Rails.application.config.FACEBOOK_APP_SECRET
-end
