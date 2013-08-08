8ff168457c68016c57a56ef2ffc759c67294ea2c
James Kobol <jim.kobol@gmail.com>
2013-08-07 18:37:04 -0700

    have a working add/remove friend; need to work on block

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,32 +1,17 @@
 class FriendshipsController < ApplicationController
-	# include Amistad::FriendModel
 	
 	def index
     @current_user = current_user
-    Rails.logger.info @current_user.friends.inspect
-    render 'users/friends'
-    # @friends = current_user.friends
-    # @pending_invited_by = current_user.pending_invited_by
-    # @pending_invited = current_user.pending_invited
-  end
+    session[:return_to] = '/friends'
 
-  def update
-    inviter = User.find_by_id(params[:id])
-    if current_user.approve inviter
-      redirect_to new_friend_path, :notice => "Successfully confirmed friend!"
-    else
-      redirect_to new_friend_path, :notice => "Sorry! Could not confirm friend!"
-    end
+    render 'users/friends'
   end
 
   def requests
 		@current_user = current_user
     @pending_requests = current_user.pending_invited_by
-    Rails.logger.info @current_user.inspect
-  end
-  
-  def invites
-    @pending_invites = current_user.pending_invited
+
+    session[:return_to] = '/friends/requests'
   end
 
   def invite
@@ -44,24 +29,24 @@ class FriendshipsController < ApplicationController
   def approve
     @friend = User.find params[:friend_id]
     @current_user = current_user
-    respond_to do |format|
+    # respond_to do |format|
 			if @current_user.approve(@friend)
-				format.html { render :nothing => true, :status => 200 }
+				redirect_to session[:return_to], notice: "You are now friends with #{@friend.name}"
 			else
-				format.html { render :nothing => true, :status => 500 }
+        redirect_to session[:return_to], notice: 'Sorry, cannot add friend'
 			end
-    end
+    # end
   end
 
   def deny
    	@friend = User.find params[:friend_id]
     @current_user = current_user
-    respond_to do |format|
+    # respond_to do |format|
 			if @current_user.remove_friendship(@friend)
-				format.html { render :nothing => true, :status => 200 }
+        redirect_to session[:return_to], notice: "You are no longer friends with #{@friend.name}"
 			else
-				format.html { render :nothing => true, :status => 500 }
+				redirect_to session[:return_to], notice: 'Sorry, cannot remove friend'
 			end
-    end
+    # end
   end
 end
