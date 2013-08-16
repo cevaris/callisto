3ce5433ec973bcb6afe418d6e54b1055916a3ddb
James Kobol <jim.kobol@gmail.com>
2013-08-16 09:09:47 -0700

    adding storyboard elements

---
+++ mobile/ios/callisto/callisto/callisto/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  callisto
+//
+//  Created by Cevaris on 8/15/13.
+//  Copyright (c) 2013 Callisto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
---
+++ mobile/ios/callisto/callisto/callisto/MyActivitiesViewController.h
@@ -0,0 +1,14 @@
+//
+//  MyActivitiesViewController.h
+//  callisto
+//
+//  Created by Cevaris on 8/15/13.
+//  Copyright (c) 2013 Callisto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "LoginViewController.h"
+
+@interface MyActivitiesViewController : UITableViewController
+
+@end
