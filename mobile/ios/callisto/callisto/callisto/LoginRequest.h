771b29b13730f9796da7b702b0613ded544b8447
James Kobol <jim.kobol@gmail.com>
2013-08-17 23:06:13 -0700

    got restkit to compile….thank god...

---
+++ mobile/ios/callisto/callisto/callisto/LoginRequest.h
@@ -0,0 +1,13 @@
+//
+//  LoginRequest.h
+//  callisto
+//
+//  Created by Cevaris on 8/17/13.
+//  Copyright (c) 2013 Callisto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LoginRequest : NSObject
+
+@end
---
+++ mobile/ios/callisto/callisto/callisto/LoginRequest.m
@@ -0,0 +1,13 @@
+//
+//  LoginRequest.m
+//  callisto
+//
+//  Created by Cevaris on 8/17/13.
+//  Copyright (c) 2013 Callisto. All rights reserved.
+//
+
+#import "LoginRequest.h"
+
+@implementation LoginRequest
+
+@end
