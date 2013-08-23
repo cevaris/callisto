8b53c1efe291d01f06441f665ba46e69cde20322
James Kobol <jim.kobol@gmail.com>
2013-08-23 17:30:08 -0600

    added ssKeychain

---
+++ mobile/ios/callisto/callisto/callisto/UserRequest.m
@@ -0,0 +1,13 @@
+//
+//  UserRequest.m
+//  callisto
+//
+//  Created by Cevaris on 8/23/13.
+//  Copyright (c) 2013 Callisto. All rights reserved.
+//
+
+#import "UserRequest.h"
+
+@implementation UserRequest
+
+@end
